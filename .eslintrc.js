a5282b8d80eec0f77819be83a9d22aa1eb1c268b
luis <lmbrmiguelito@hotmail.com>
2018-06-10 11:13:40 -0500

    changes until the passport video in Building web aplications with node and express 4.0

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "airbnb-base",
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "comma-dangle": 0
+    }
+};
